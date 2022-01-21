121ca3780fd7b1d11456d3c93c4e450db6457adb
Dave Compton <davecompton7@gmail.com>
2022-01-21 13:42:01 -0800

    Using js-yaml to decode yaml.

--- sample_document.ts
+++ sample_document.ts
@@ -1,13 +1,12 @@
-'use strict';
-
 const util = require('util');
+var yaml = require('js-yaml');
 require('isomorphic-fetch');
-
 
 async function toplev() {
     try {
       const resp = await fetch("https://raw.githubusercontent.com/dc25/sl/main/sample_document.yml");
-      const data = await resp.text();
+      const contents = await resp.text();
+      const data = await yaml.load(contents);
 
       console.log(util.inspect(data, false, 10, true));
     } catch (err) {
